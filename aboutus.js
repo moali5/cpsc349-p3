3d3520b06f073be0d5d0a68d4e37e4786b32415f
Mohammed_Ali <moali5@csu.fullerton.edu>
2022-11-26 11:57:03 -0800

    fixing the buttons

---
+++ aboutus.js
@@ -0,0 +1,12 @@
+
+
+
+const btn1 = document.querySelector('.button1');
+
+
+
+btn1.addEventListener('click', () => {
+
+window.location.assign('/index.html');
+
+})
---
+++ game.js
@@ -0,0 +1,11 @@
+
+const btn1 = document.querySelector('.button1');
+
+
+
+btn1.addEventListener('click', () => {
+
+window.location.assign('/index.html');
+
+})
+
