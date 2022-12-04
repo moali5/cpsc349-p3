0489abe5b5408e8c4c155f3b2d51619f8b809fb1
Mohammed_Ali <moali5@csu.fullerton.edu>
2022-12-03 21:26:29 -0800

    final fix

--- game.js
+++ game.js
@@ -30,7 +30,7 @@ const hideBtn = document.querySelector('#hideButton')
 const btn2 = document.querySelector('.button2')
 const idContinue = document.querySelector('#continue')
 
-//=====================================================================
+//= ====================================================================
 
 btn1.addEventListener('click', () => {
   idIndexPage.classList.remove('hidden')
@@ -43,10 +43,9 @@ btn1.addEventListener('click', () => {
   idIndexPage1.classList.remove('hidden')
   idAboutPage1.classList.add('hidden')
   idGamePage1.classList.add('hidden')
-
 })
 
-//=================================================================
+//= ================================================================
 
 btn2.addEventListener('click', () => {
   idIndexPage.classList.add('hidden')
@@ -61,10 +60,39 @@ btn2.addEventListener('click', () => {
   idGamePage1.classList.add('hidden')
 })
 
-//==================================================================
+//= =================================================================
 
-btn4.addEventListener('click', () =>
-{
+btn4.addEventListener('click', () => {
+  idIndexPage.classList.add('hidden')
+  idAboutPage.classList.add('hidden')
+  idGamePage.classList.remove('hidden')
+
+  hideBtn.classList.add('hidden')
+  btn3.classList.remove('hidden')
+
+  idIndexPage1.classList.add('hidden')
+  idAboutPage1.classList.add('hidden')
+  idGamePage1.classList.remove('hidden')
+})
+
+//= =========================================================================
+/* try to use the local storage not working
+when i check the DOM look like its storing
+the date. but one pulling the date
+with localStorage.getItem() will work on it on saturday
+see if you can fix the issue */
+// const idContinue = document.querySelector('#continue')
+
+idContinue.addEventListener('click', () => {
+  id.classList.remove('hidden')
+
+  for (let i = 0; i < id2.length; i++) {
+    id2[i].classList.remove('hidden')
+  }
+  for (let i = 0; i < id3.length; i++) {
+    id3[i].classList.remove('hidden')
+  }
+
   idIndexPage.classList.add('hidden')
   idAboutPage.classList.add('hidden')
   idGamePage.classList.remove('hidden')
@@ -76,210 +104,152 @@ btn4.addEventListener('click', () =>
   idAboutPage1.classList.add('hidden')
   idGamePage1.classList.remove('hidden')
 
+  text1.value = localStorage.getItem('name')
+  totalAi = JSON.parse(localStorage.getItem('Ai'))
+  totalHuman = JSON.parse(localStorage.getItem('human'))
+
+  winMess1 = text1.value
+  pName.innerHTML = text1.value
+  winMess2.textContent = totalHuman
+
+  winMess3.textContent = totalAi
 })
 
+//= ==========================================================================
 
-
-//==========================================================================
-/* try to use the local storage not working
-when i check the DOM look like its storing
-the date. but one pulling the date
-with localStorage.getItem() will work on it on saturday
-see if you can fix the issue*/
-//const idContinue = document.querySelector('#continue')
-
-idContinue.addEventListener('click', () =>{
-      id.classList.remove('hidden')
-
-    for(let i=0; i <id2.length; i++)
-    {
-      id2[i].classList.remove('hidden')
-    }
-    for(let i=0; i <id3.length; i++)
-    {
-      id3[i].classList.remove('hidden')
-    }
-
-    idIndexPage.classList.add('hidden')
-  idAboutPage.classList.add('hidden')
-  idGamePage.classList.remove('hidden')
-
-  hideBtn.classList.add('hidden')
-  btn3.classList.remove('hidden')
-
-  idIndexPage1.classList.add('hidden')
-  idAboutPage1.classList.add('hidden')
-  idGamePage1.classList.remove('hidden')
-
-    text1.value = localStorage.getItem("name")
-    totalAi =JSON.parse(localStorage.getItem("Ai"))
-    totalHuman = JSON.parse(localStorage.getItem("human"))
-
-    winMess1 = text1.value
-      pName.innerHTML = text1.value
-    winMess2.textContent = totalHuman
-
-     winMess3.textContent = totalAi
-
-
-})
-
-
-
-
-//===========================================================================
-
-
-btn3.addEventListener('click', () =>
-{
-
-
+btn3.addEventListener('click', () => {
   id.classList.remove('hidden')
 
-  if((totalAi > 0) && (totalHuman > 0))
-  {
-    for(let i=0; i <id2.length; i++)
-    {
+  if ((totalAi > 0) && (totalHuman > 0)) {
+    for (let i = 0; i < id2.length; i++) {
       id2[i].classList.add('hidden')
     }
-    for(let i=0; i <id3.length; i++)
-    {
+    for (let i = 0; i < id3.length; i++) {
       id3[i].classList.add('hidden')
     }
-      /*   totalAi = 0
+    /*   totalAi = 0
          totalHuman = 0
          winMess2.textContent = ""
          winMess3.textContent = ""
-         winMess1.textContent = ""*/
+         winMess1.textContent = "" */
   }
-  winMess4.textContent = "Please Enter"
+  winMess4.textContent = 'Please Enter'
   const li1 = document.createElement('li')
-  li1.textContent = "Your Name"
+  li1.textContent = 'Your Name'
   winMess4.appendChild(li1)
   const li2 = document.createElement('li')
-  li2.textContent = "Then Click"
+  li2.textContent = 'Then Click'
   winMess4.appendChild(li2)
   const li3 = document.createElement('li')
-  li3.textContent = "The Submit Button"
+  li3.textContent = 'The Submit Button'
   winMess4.appendChild(li3)
   const li4 = document.createElement('li')
-  li4.textContent = "To Start New Game"
+  li4.textContent = 'To Start New Game'
   winMess4.appendChild(li4)
-
-
 })
 
-
-
-//====================================rock=============================
-
+//= ===================================rock=============================
 
 rock.addEventListener('click', () => {
   const computer = Math.floor(Math.random() * 3) + 1
 
-  winImage[1].src= '/cpsc349-p3/rock.png'
+  winImage[1].src = '/cpsc349-p3/rock.png'
 
-  if (id3[0].classList.contains('hidden')){
-  for(let i=0; i <id3.length; i++)
- {
-  id3[i].classList.remove('hidden')
- }}
+  if (id3[0].classList.contains('hidden')) {
+    for (let i = 0; i < id3.length; i++) {
+      id3[i].classList.remove('hidden')
+    }
+  }
 
   if (computer === 1) {
     totalAi += 1
     totalHuman += 1
-    winMess1.textContent = ""
+    winMess1.textContent = ''
     winMess.textContent = 'Its a Draw'
     winMess2.textContent = totalHuman
     winMess3.textContent = totalAi
-    winImage[2].src= '/cpsc349-p3/rock.png'
+    winImage[2].src = '/cpsc349-p3/rock.png'
   } else if (computer === 2) {
     totalHuman += 2
     winMess1.textContent = text1.value
     winMess.textContent = 'you win'
     winMess2.textContent = totalHuman
     winMess3.textContent = totalAi
-    winImage[2].src= '/cpsc349-p3/scissor.png'
+    winImage[2].src = '/cpsc349-p3/scissor.png'
   } else if (computer === 3) {
     totalAi += 2
-    winMess1.textContent = ""
+    winMess1.textContent = ''
     winMess.textContent = 'AI wins'
     winMess2.textContent = totalHuman
     winMess3.textContent = totalAi
-    winImage[2].src= '/cpsc349-p3/paper.png'
+    winImage[2].src = '/cpsc349-p3/paper.png'
   }
 
   localStorage.setItem('Ai', totalAi)
   localStorage.setItem('human', totalHuman)
-
-
 })
 
-
-
-//=========================paper===========================================
-
+//= ========================paper===========================================
 
 paper.addEventListener('click', () => {
   const computer = Math.floor(Math.random() * 3) + 1
 
-  winImage[1].src= '/cpsc349-p3/paper.png'
+  winImage[1].src = '/cpsc349-p3/paper.png'
 
-  if (id3[0].classList.contains('hidden')){
-  for(let i=0; i <id3.length; i++)
-  {
-    id3[i].classList.remove('hidden')
-  }}
+  if (id3[0].classList.contains('hidden')) {
+    for (let i = 0; i < id3.length; i++) {
+      id3[i].classList.remove('hidden')
+    }
+  }
 
   if (computer === 2) {
     totalAi += 2
-    winMess1.textContent = ""
+    winMess1.textContent = ''
     winMess.textContent = 'Ai Wins'
     winMess2.textContent = totalHuman
     winMess3.textContent = totalAi
-    winImage[2].src= '/cpsc349-p3/scissor.png'
+    winImage[2].src = '/cpsc349-p3/scissor.png'
   } else if (computer === 1) {
     totalHuman += 2
     winMess1.textContent = text1.value
     winMess.textContent = 'you win'
     winMess2.textContent = totalHuman
     winMess3.textContent = totalAi
-    winImage[2].src= '/cpsc349-p3/rock.png'
+    winImage[2].src = '/cpsc349-p3/rock.png'
   } else if (computer === 3) {
     totalAi += 1
     totalHuman += 1
-    winMess1.textContent = ""
+    winMess1.textContent = ''
     winMess.textContent = 'its a Draw'
     winMess2.textContent = totalHuman
     winMess3.textContent = totalAi
-    winImage[2].src= '/cpsc349-p3/paper.png'
+    winImage[2].src = '/cpsc349-p3/paper.png'
   }
 
-   localStorage.setItem('Ai', totalAi)
-   localStorage.setItem('human', totalHuman)
+  localStorage.setItem('Ai', totalAi)
+  localStorage.setItem('human', totalHuman)
 })
 
-
-//========================================scissor============================
+//= =======================================scissor============================
 
 scissor.addEventListener('click', () => {
   const computer = Math.floor(Math.random() * 3) + 1
 
-  winImage[1].src= '/cpsc349-p3/scissor.png'
+  winImage[1].src = '/cpsc349-p3/scissor.png'
 
-  if (id3[0].classList.contains('hidden')){
-  for(let i=0; i <id3.length; i++)
-  {
-    id3[i].classList.remove('hidden')
-  }}
+  if (id3[0].classList.contains('hidden')) {
+    for (let i = 0; i < id3.length; i++) {
+      id3[i].classList.remove('hidden')
+    }
+  }
 
   if (computer === 3) {
     totalHuman += 2
-    winMess1.textContent = ""
+    winMess1.textContent = ''
     winMess.textContent = 'You win'
     winMess2.textContent = totalHuman
     winMess3.textContent = totalAi
-    winImage[2].src= '/cpsc349-p3/paper.png'
+    winImage[2].src = '/cpsc349-p3/paper.png'
   } else if (computer === 2) {
     totalHuman += 1
     totalAi += 1
@@ -287,24 +257,21 @@ scissor.addEventListener('click', () => {
     winMess1.textContent = text1.value
     winMess2.textContent = totalHuman
     winMess3.textContent = totalAi
-    winImage[2].src= '/scissor.png'
+    winImage[2].src = '/scissor.png'
   } else if (computer === 1) {
     totalAi += 2
-    winMess1.textContent = ""
+    winMess1.textContent = ''
     winMess.textContent = 'AI wins'
     winMess2.textContent = totalHuman
     winMess3.textContent = totalAi
-    winImage[2].src= '/cpsc349-p3/rock.png'
+    winImage[2].src = '/cpsc349-p3/rock.png'
   }
 
   localStorage.setItem('Ai', totalAi)
   localStorage.setItem('human', totalHuman)
 })
 
-
-//=======================================================================
-
-
+//= ======================================================================
 
 const btn = document.querySelector('.button4')
 
@@ -317,38 +284,32 @@ btn.addEventListener('click', () => {
   document.body.style.backgroundColor = rndCol
 })
 
+//= ==========================================================================
 
+btn6.addEventListener('click', () => {
+  pName.innerHTML = text1.value
 
+  localStorage.setItem('name', text1.value)
 
-//===========================================================================
+  for (let i = 0; i < id2.length; i++) {
+    id2[i].classList.remove('hidden')
+  }
 
-btn6.addEventListener('click', () =>
-{
- pName.innerHTML = text1.value
-
- localStorage.setItem('name', text1.value)
-
- for(let i=0; i <id2.length; i++)
- {
- id2[i].classList.remove('hidden')
- }
-
-
- id.classList.add('hidden')
- winMess4.textContent = 'Please Select'
- const li1 = document.createElement('li')
- li1.textContent = "ROCK"
- winMess4.appendChild(li1)
- const li2 = document.createElement('li')
- li2.textContent = "PAPER"
- winMess4.appendChild(li2)
- const li3 = document.createElement('li')
- li3.textContent = "SCISSOR"
- winMess4.appendChild(li3)
+  id.classList.add('hidden')
+  winMess4.textContent = 'Please Select'
+  const li1 = document.createElement('li')
+  li1.textContent = 'ROCK'
+  winMess4.appendChild(li1)
+  const li2 = document.createElement('li')
+  li2.textContent = 'PAPER'
+  winMess4.appendChild(li2)
+  const li3 = document.createElement('li')
+  li3.textContent = 'SCISSOR'
+  winMess4.appendChild(li3)
 })
 /*
 text1.value = JSON.parse(localStorage.getItem("name"))
 totalAi = JSON.parse(localStorage.getItem("ai"))
 totalHuman = JSON.parse(localStorage.getItem("human"))
 
-*///====================== slide over
+*///= ===================== slide over
